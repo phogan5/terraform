2e64a50490aebefd6e540d27475134f5ea5f36f7
Philip <philiphogan10@gmail.com>
2023-06-21 14:27:13 -0400

    changing to relative file locations

--- homelab/main.tf
+++ homelab/main.tf
@@ -14,24 +14,24 @@ terraform {
 }
 
 module "remote_datastore" {
-    source = "/Users/phogan5/terraform/homelab/data_stores"
+    source = "./data_stores"
 }
 
 module "homelab_network" {
-    source = "/Users/phogan5/terraform/homelab/modules/network"
+    source = "./modules/network"
 
     vpc_id = "vpc-08b6a277deb2d6942"
 }
 
 module "linux_server" {
-    source = "/Users/phogan5/terraform/homelab/modules/linux_server"
+    source = "./modules/linux_server"
 
     sg_id = "${module.homelab_network.sg_id}"
     subnet_id = "subnet-0e1767c810b88f50e"
 }
 
 module "windows_server" {
-    source = "/Users/phogan5/terraform/homelab/modules/windows_server"
+    source = "./modules/windows_server"
 
     sg_id = "${module.homelab_network.sg_id}"
     subnet_id = "subnet-0e1767c810b88f50e"
