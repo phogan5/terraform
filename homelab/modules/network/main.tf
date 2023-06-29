0f062e4dd8d1672a93daceb2b8bb72781c434f2a
phogan5 <philiphogan10@gmail.com>
2023-06-28 20:19:51 -0400

    secure vpc + IPAM pool

--- homelab/modules/network/main.tf
+++ homelab/modules/network/main.tf
@@ -2,6 +2,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc_ipam" "tf_ipam" {
+    operating_regions {
+        region_name = "us-east-1"
+    }
+}
+
+resource "aws_vpc_ipam_pool" "tf_ipam_pool" {
+    address_family = "ipv4"
+    ipam_scope_id = aws_vpc_ipam.tf_ipam.private_default_scope_id
+    locale = "us-east-1"
+}
+
+resource "aws_vpc_ipam_pool_cidr" "tf_ipam_cidr" {
+    ipam_pool_id = aws_vpc_ipam_pool.tf_ipam_pool.id
+    cidr = "172.2.0.0/16"
+  
+}
+
+resource "aws_vpc" "tf_vpc" {
+    ipv4_ipam_pool_id = aws_vpc_ipam_pool.tf_ipam_pool.id
+    ipv4_netmask_length = 25
+    depends_on = [aws_vpc_ipam_pool_cidr.tf_ipam_cidr]
+
+    tags = {
+      "Name" = "secure_vpc"
+    }
+}
+
 resource "aws_security_group" "homelab-sg" {
     name = "homelab-sg"
     vpc_id = var.vpc_id
--- homelab/modules/network/variables.tf
+++ homelab/modules/network/variables.tf
@@ -1,4 +1,5 @@
 variable "vpc_id" {
+    default = "vpc-08b6a277deb2d6942"
     type = string
     description = "The ID of the VPC to deploy instances in"
 }
