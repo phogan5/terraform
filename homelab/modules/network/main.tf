67c70189deb12c6cf3315cd787f2ddb41031dca7
phogan5 <philiphogan10@gmail.com>
2023-06-28 22:24:18 -0400

    secure vpc subnets

--- homelab/modules/network/main.tf
+++ homelab/modules/network/main.tf
@@ -2,33 +2,71 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc_ipam" "tf_ipam" {
-    operating_regions {
-        region_name = "us-east-1"
-    }
-}
-
-resource "aws_vpc_ipam_pool" "tf_ipam_pool" {
-    address_family = "ipv4"
-    ipam_scope_id = aws_vpc_ipam.tf_ipam.private_default_scope_id
-    locale = "us-east-1"
-}
-
-resource "aws_vpc_ipam_pool_cidr" "tf_ipam_cidr" {
-    ipam_pool_id = aws_vpc_ipam_pool.tf_ipam_pool.id
-    cidr = "172.2.0.0/16"
-  
-}
-
 resource "aws_vpc" "tf_vpc" {
-    ipv4_ipam_pool_id = aws_vpc_ipam_pool.tf_ipam_pool.id
-    ipv4_netmask_length = 25
-    depends_on = [aws_vpc_ipam_pool_cidr.tf_ipam_cidr]
-
+    cidr_block = "10.0.0.0/16"
     tags = {
       "Name" = "secure_vpc"
     }
 }
+
+resource "aws_internet_gateway" "tf_igw" {
+    vpc_id = aws_vpc.tf_vpc.id
+}
+
+resource "aws_subnet" "tf_subnet1" {
+    vpc_id = aws_vpc.tf_vpc.id
+    cidr_block = "10.0.0.0/20"
+    availability_zone = "us-east-1a"
+
+    tags = {
+      "Name" = "private-1"
+    }
+}
+resource "aws_subnet" "tf_subnet2" {
+    vpc_id = aws_vpc.tf_vpc.id
+    cidr_block = "10.0.16.0/20"
+    availability_zone = "us-east-1b"
+    tags = {
+      "Name" = "private-2"
+    }
+}
+resource "aws_subnet" "tf_subnet3" {
+    vpc_id = aws_vpc.tf_vpc.id
+    cidr_block = "10.0.32.0/20"
+    availability_zone = "us-east-1c"
+
+    tags = {
+      "Name" = "private-3"
+    }
+}
+resource "aws_subnet" "tf_subnet4" {
+    vpc_id = aws_vpc.tf_vpc.id
+    cidr_block = "10.0.48.0/20"
+    availability_zone = "us-east-1d"
+
+    tags = {
+      "Name" = "public-1"
+    }
+}
+resource "aws_subnet" "tf_subnet5" {
+    vpc_id = aws_vpc.tf_vpc.id
+    cidr_block = "10.0.64.0/20"
+    availability_zone = "us-east-1e"
+
+    tags = {
+      "Name" = "public-2"
+    }
+}
+resource "aws_subnet" "tf_subnet6" {
+    vpc_id = aws_vpc.tf_vpc.id
+    cidr_block = "10.0.80.0/20"
+    availability_zone = "us-east-1f"
+
+    tags = {
+      "Name" = "public-3"
+    }
+}
+
 
 resource "aws_security_group" "homelab-sg" {
     name = "homelab-sg"
