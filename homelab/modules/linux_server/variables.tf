003b428673b8d8ac691df2c01d29fdb6c161f510
Philip <philiphogan10@gmail.com>
2023-06-29 20:06:50 -0400

    eip

--- homelab/modules/linux_server/variables.tf
+++ homelab/modules/linux_server/variables.tf
@@ -1,10 +1,18 @@
 variable "sg_id" {
     type = string
     description = "The ID of the security group created for the RHEL and Windows servers"
+    #secure-vpc default sg
+    default = "sg-0054047ff951519f4"
 }
 
 variable "subnet_id" {
     type = string
     description = "The ID of the subnet used when creating the RHEL and Windows servers"
-    default = "subnet-0e1767c810b88f50e"
+    default = "subnet-092a6e9b543ef5ab2"
+}
+
+variable "instance_type" {
+    type = string
+    description = "The size of the EC2 instance"
+    default = "t2.micro"
 }
