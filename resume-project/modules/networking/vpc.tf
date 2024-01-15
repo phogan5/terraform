1d8e335904c8db89bd8b6d265ac4e16c2416e287
phogan5 <philiphogan10@gmail.com>
2024-01-15 15:00:42 -0500

    initial commit

---
+++ resume-project/modules/networking/vpc.tf
@@ -0,0 +1,41 @@
+resource "aws_vpc" "primary_vpc" {
+    cidr_block = var.primary_vpc_cidr
+    instance_tenancy = default
+    tags = {
+        Name = "web_app_vpc"
+        Description = "The primary VPC for web app testing. Built using terraform from https://github.com/phogan5/terraform"
+    }
+}
+
+resource "aws_subnet" "public_subnet_az1" {
+  vpc_id = aws_vpc.primary_vpc.id
+  cidr_block = var.public_subnet_1_cidr
+  availability_zone = var.az1
+  tags = {
+    Name = self.Name
+  }
+}
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id = aws_vpc.primary_vpc.id
+  cidr_block = var.public_subnet_2_cidr
+  availability_zone = var.az2
+  tags = {
+    Name = self.Name
+  }
+}
+resource "aws_subnet" "private_subnet_az1" {
+  vpc_id = aws_vpc.primary_vpc.id
+  cidr_block = var.private_subnet_1_cidr
+  availability_zone = var.az1
+  tags = {
+    Name = self.Name
+  }
+}
+resource "aws_subnet" "private_subnet_az2" {
+  vpc_id = aws_vpc.primary_vpc.id
+  cidr_block = var.private_subnet_2_cidr
+  availability_zone = var.az2
+  tags = {
+    Name = self.Name
+  }
+}
---
+++ resume-project/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "remote" {
+    organization = "example-org-4de848"
+
+    workspaces {
+      name = "generic-workspace"
+    }
+  }
+  required_providers {
+    aws = {
+        version = "~>5.0"
+        source = "hashicorp/aws"
+    }
+  }
+}
+
+  provider "aws" {
+    region = "us-east-1"
+  }
